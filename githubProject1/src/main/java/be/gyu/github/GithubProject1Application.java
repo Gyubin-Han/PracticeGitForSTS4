aa36bec391e67ae036bebf6b24ce06e08a14ab85
Gyubin Han <yangglei2@naver.com>
2023-12-29 16:55:26 +0900

    init commit for STS Project

---
+++ githubProject1/src/main/java/be/gyu/github/GithubProject1Application.java
@@ -0,0 +1,13 @@
+package be.gyu.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubProject1Application.class, args);
+	}
+
+}
---
+++ githubProject1/src/test/java/be/gyu/github/GithubProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.gyu.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
